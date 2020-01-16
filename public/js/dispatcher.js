0836231ed8d9cee61c7fa280d05e479274df963b
Thomas Lind <thomas.lind@it.uu.se>
2020-01-16 14:03:09 +0100

    Add linting with eslint using strongloop formatting and the eslint plugin for vue. Project linted.

--- public/js/dispatcher.js
+++ public/js/dispatcher.js
@@ -1,21 +1,22 @@
-/*jslint es5:true, indent: 2 */
-/*global Vue, io */
+/* jslint es6:true, indent: 2 */
+/* global Vue, io */
 /* exported vm */
 'use strict';
 const socket = io();
 
+/* eslint-disable-next-line no-unused-vars */
 const vm = new Vue({
   el: '#orders',
   data: {
     orders: {},
   },
-  created: function () {
-    socket.on('initialize', function (data) {
+  created: function() {
+    socket.on('initialize', function(data) {
       this.orders = data.orders;
     }.bind(this));
 
-    socket.on('currentQueue', function (data) {
+    socket.on('currentQueue', function(data) {
       this.orders = data.orders;
     }.bind(this));
-  }
+  },
 });
